d3e84acb96010cd215af0c224ad3cc7ac88bc516
Joel Delight and Terri Bradley <terri@>
2016-06-08 09:40:45 -0700

    added logic for non numbers and numbers divisible by 400.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,37 @@
+var allowableInputs = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+
 var leapYear = function(num) {
-  if (num % 4 === 0 && num % 100 != 0) {
+  var boolean = true
+  var numLength =num.length;
+  for (i=0; i < numLength; i ++) {
+    if (allowableInputs.includes(num.charAt(i))) {}
+    else {
+      boolean = false
+    }
+  };
+  var number = parseInt(num);
+  if (!boolean) {
+    alert ("unallowed input")
+    return false;
+  }
+  else if (number % 4 === 0 && number % 100 != 0 || number % 400 === 0) {
     return true;
   } else {
   return false;
   }
 
-}
+};
 
 
-
-
-$(document).ready(function(){
-  $(".year-form#submit").submit(function(event) {
+$(document).ready(function() {
+  $(".year-form").submit(function(event) {
     event.preventDefault();
     var year = $("#year-input").val();
-    leapYear(year);
-  })
+    if (leapYear(year)) {
+      $("#result").text(year + " is a leap year!")
+    } else {
+      $("#result").text(year + " is not a leap year.")
+    }
+  });
+
 });
