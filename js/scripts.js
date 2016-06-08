6b485792499d54af275d08148c0d62954ce26448
Joel Delight and Terri Bradley <terri@>
2016-06-08 08:42:21 -0700

    allow the result to return true on a year divisible by 4.

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+var leapYear = function(num) {
+  if (num % 4 === 0) {
+    return true;
+  } else {
+  return false;
+  }
+}
+
+
+
+
+$(document).ready(function(){
+  $(".year-form#submit").submit(function(event) {
+    event.preventDefault();
+    var year = $("#year-input").val();
+    leapYear(year);
+  })
+});
